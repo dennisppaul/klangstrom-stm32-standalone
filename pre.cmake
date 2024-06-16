f42d8d2e38024510463a8d6b1fc5fd1d1e3dd752
dennisppaul <dennis.paul@hfk-bremen.de>
2024-06-16 22:12:01 +0200

    cleaned up CMake

---
+++ pre.cmake
@@ -0,0 +1,7 @@
+enable_language(C)
+enable_language(CXX)
+enable_language(ASM)
+
+# Set C and C++ standards
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_C_STANDARD 11)
---
+++ toolchain.cmake
@@ -0,0 +1,1 @@
+set(CMAKE_TOOLCHAIN_FILE "${LIBRARY_PATH}/arm-none-eabi-toolchain.cmake" CACHE STRING "Toolchain file") 
